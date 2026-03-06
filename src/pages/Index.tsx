a9b8f2be5dcc208bf397d105a458e93e4f8d4e07
Sanskar Gohil <gohilsanskar1203@gmail.com>
2026-03-06 14:14:36 +0530

    added more sections

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,18 @@
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import FareComparison from "../components/FareComparision";
+import FeaturesSection from "../components/FeaturesSection";
+import CitiesSection from "../components/CitiesSection";
+
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <HeroSection />
+      <FareComparison/>
+      <FeaturesSection/>
+      <CitiesSection/>
     </div>
   );
 };
